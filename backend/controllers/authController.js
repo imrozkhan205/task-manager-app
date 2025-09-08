4d3bf02b57c93ecde4eb7ba81e56de070084aa93
imrozkhan205 <imrozkhan2258@gmail.com>
2025-09-08 18:39:59 +0530

    Imported dotenv and did dotenv.config maybe that could solve it

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,9 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const signToken = (userId) => {
   const secret = process.env.JWT_SECRET;
